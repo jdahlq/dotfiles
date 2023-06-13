b46b9361a1284d3bb16d2b863e8b7558ec3a6322
Joe Dahlquist <joe@dahlquist.wtf>
2023-06-12 22:35:24 -0700

    add gitconfig to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #! /bin/zsh
 
-cp .zshrc ~/.
+# # Install Starship:
+# # Mac: brew install cmake
+# # Ubuntu: apt install cmake
+# cargo install starship --locked
+
+cp .gitconfig ~/
+cp .zshrc ~/
 cp kitty.conf ~/.config/kitty/
